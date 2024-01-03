d477e4ba99699ab30dea06f10352da042e456732
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-01-03 04:44:48 +0100

    0x00

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if there is a cycle in a linked list
+ * @list: linked list to check
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *s = list;
+	listint_t *f = list;
+
+	if (!list)
+		return (0);
+
+	while (s && f && f->next)
+	{
+		s = s->next;
+		f = f->next->next;
+		if (s == f)
+			return (1);
+	}
+	return (0);
+}
