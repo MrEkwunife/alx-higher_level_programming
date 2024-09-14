09092027605165798dc25ea30a9c270cd06a7f50
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-09-14 07:07:50 +0100

    3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for li in my_list[::-1]:
-        print("{:d}".format(li))
+    if isinstance(my_list, list):
+        for m in my_list[::-1]:
+            print("{:d}".format(m))
