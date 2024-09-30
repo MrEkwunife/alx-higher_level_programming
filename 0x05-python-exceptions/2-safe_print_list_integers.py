5961c76844f6f652bbf9abe645c95a542362a265
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-09-30 21:19:59 +0100

    safe

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    pl = 0
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            pl += 1
+        except (ValueError, TypeError):
+            continue
+
+    print("")
+    return pl
