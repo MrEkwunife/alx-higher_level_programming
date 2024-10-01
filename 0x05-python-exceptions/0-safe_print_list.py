427206a98a3e04bc248a219d15a42464741dedd9
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-10-01 05:06:55 +0100

    ex

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0):
-    pit = 0
 
+def safe_print_list(my_list=[], x=0):
+    ret = 0
     for i in range(x):
         try:
-            print(my_list[i], end='')
-            pit += 1
+            print("{}".format(my_list[i]), end="")
+            ret += 1
         except IndexError:
             break
-    print("")
-    return pit
+    print()
+    return (ret)
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_division(a, b):
     try:
         div = a / b
@@ -7,4 +8,4 @@ def safe_print_division(a, b):
         div = None
     finally:
         print("Inside result: {}".format(div))
-    return div
+    return (div)
