22db4894e32fa05fdb339b54f3c5599e0c683e1e
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-10-09 05:14:25 +0100

    tdd

---
+++ 0x07-python-test_driven_development/102-compile.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/fish
+# set PY_BIN_PATH /usr/bin/python3.4
+# set PY_INC_PATH /usr/include/python3.4
+set PY_BIN_PATH ~/apps/Python-3.4.10/python
+set PY_INC_PATH ~/apps/Python-3.4.10/Include/:"$HOME/apps/Python-3.4.10/"
+gcc -Wall -Werror -Wextra -pedantic -std=c99 -shared -Wl,-soname,libPython.so -o libPython.so -fPIC -I$PY_INC_PATH 102-python.c
+# [ $status -eq 0 ] && $PY_BIN_PATH ./102-python.py
