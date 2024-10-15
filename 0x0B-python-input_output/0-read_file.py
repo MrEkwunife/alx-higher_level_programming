538a717080d3934ec5d4133196f9d3b173553bb7
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-10-15 16:08:14 +0100

    r

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, 'r', encoding='utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         print(f.read(), end='')
