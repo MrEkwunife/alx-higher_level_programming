7a35fa971a85ab5a8688bb4416cc99538a2cfb5c
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-10-15 16:30:18 +0100

    writes

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Contains a function that writes to a file"""
+
+
 def write_file(filename="", text=""):
+    """Writes in utf-8 into a file"""
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
