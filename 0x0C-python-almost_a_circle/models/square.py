f826c5704010dd773a514556d6d97a1b91e45c27
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-10-21 23:22:23 +0100

    size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,6 +18,17 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Gets the size of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Returns the string rep of the square"""
         return "[Square] ({}) {}/{} - {}".format(
