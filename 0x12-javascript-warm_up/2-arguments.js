446c6e6c6ce2e180179439893758eafbb81e8369
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-12-16 15:29:31 +0100

    args found

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-
-const { argv } = require('node:process');
-
-if (argv.length <= 2) {
-  console.log('No argument');
-} else {
-  console.log('Argument found');
-}
+const count = process.argv.length
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found')
