06f7d4a8ca4007b285731a725ef2240d6443f399
Chimobi <ekwunifechimobi16@gmail.com>
2025-03-03 16:50:52 +0100

    2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
--- create a database and give user select access to it
+-- Creates a database in the server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- Creates a user with Select access
+-- Creates a user with SELECT privileges only
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2 TO user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
